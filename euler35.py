2164db6f9035cebb8e1d1c253474b23d79f2dea1
Devin Kelly <dwwkelly@gmail.com>
2012-11-04 17:53:10 -0500

    solved #35

--- euler35.py
+++ euler35.py
@@ -5,19 +5,23 @@ from eulerlib import rotateInt
 from eulerlib import nDigits
 
 primes = sieveOfEratosthenes(1000000)
-circPrimes = []
+#primes = sieveOfEratosthenes(100)
+ncircPrimes = 0
+
+digits = [[], [], [], [], [], []]
 
 for ii in primes:
-   iiLength = nDigits( ii )
+    n = nDigits( ii )
+    digits[n - 1].append( ii )
 
-   for kk in range( iiLength ):
-      rotated = rotateInt( ii, kk )
-      if rotated not in primes:
-         continue
-      else:
-         primes.remove( rotated )
+for n in range(len(digits)):  # Loop over each bucket
+    print n
+    for m in digits[n]:  # Loop over the integers in each bucket
+        isCircPrime = True
+        for l in range(n + 1):  # Loop over the rotations of each number
+            if rotateInt(m, l) not in digits[n]:
+                break
+        else:
+            ncircPrimes = ncircPrimes + 1
 
-      if kk == iiLength - 1:
-         circPrimes.append( ii )
-
-print len( circPrimes )
+print ncircPrimes
